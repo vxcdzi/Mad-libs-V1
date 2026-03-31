d6cdb81c1cb0b11aa9e3bcf8b74b08aefcf774ef
vxcdzi <arnav.shrivastav994@gmail.com>
2026-03-31 11:11:18 +0530

    Add files via upload

---
+++ madlibs-v1.py
@@ -0,0 +1,16 @@
+#Madlibs V1
+print("====Madlibs Game====")
+adjective1=input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2=input("Enter an adjective: ")
+verb1=input("Enter a verb (ending with -ing): ")
+adjective3=input("Enter an adjective: ")
+
+
+print(f"Today I went to a {adjective1} amusement park")
+print()
+print(f"Inside the park, I saw {noun1} ")
+print()
+print(f"{noun1} was {adjective2} and {verb1}")
+print()
+print(f"Their actions made me {adjective3}")
